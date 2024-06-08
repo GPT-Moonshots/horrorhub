5ec7b3e55a610755d1dfb914cdaf78849c335305
Pudv95 <vivekupadhayay18@gmail.com>
2024-06-08 13:10:38 +0530

    change theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 ThemeData myTheme = ThemeData(
-  colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.brown)
+  colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red)
       .copyWith(secondary: Colors.orange),
 );
